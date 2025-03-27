7af199a7b434e7df09e59d617fee8a72328b57c3
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-03-26 23:42:28 -0400

    update

--- main.py
+++ main.py
@@ -2,4 +2,25 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-resources = CoffeeMaker().report()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+CoffeeMaker().report()
+coffes = Menu().get_items()
+
+is_on = True
+while is_on:
+    coffees = menu.get_items()
+    user_choice = input(f"chose your coffe {coffees} or 'report': ")
+
+    if user_choice=='off':
+        is_on = False
+    elif user_choice=='report':
+        coffee_maker.report()
+        money_machine.report()
+    # else:
+    #     order = Menu().find_drink(user_choice)
+    #     if order!=None:
+    #         if ==True:
+    #
